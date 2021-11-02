0838f25a92d5a978cfb3be1f521cfa54a3aafcaf
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-11-01 22:27:51 -0400

    4

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,20 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY';
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
 export const addOne = () => {
-    return({type:ADD_ONE});
+    return({type: ADD_ONE});
 }
 
-export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
+export const applyNumber = (num) => {
+    return({type: APPLY_NUMBER, payload: num});
+}
+
+export const changeOperation = (operation) => {
+    return({type: CHANGE_OPERATION, payload: operation});
+}
+
+export const clearDisplay = () => {
+    return({type: CLEAR_DISPLAY});
 }
